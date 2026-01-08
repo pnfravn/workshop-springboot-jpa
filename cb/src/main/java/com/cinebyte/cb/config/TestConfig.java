97f9ed8296b951b5983295adea8271dde442e24c
Vitor Teixeira <neferpitou995@gmail.com>
2026-01-08 18:30:51 -0300

    Many-to-many association with JoinTable

--- cb/src/main/java/com/cinebyte/cb/config/TestConfig.java
+++ cb/src/main/java/com/cinebyte/cb/config/TestConfig.java
@@ -55,6 +55,15 @@ public class TestConfig implements CommandLineRunner{
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "maria Brown", "maria@gmail.com", "99888888", "123456");
 		User u2 = new User(null, "alex Brwon", "alex@gmail.com", "99777777","123456");
 		
