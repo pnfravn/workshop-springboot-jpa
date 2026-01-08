3404786b439e566fe8514c8c1be3dcb095c7de79
Vitor Teixeira <neferpitou995@gmail.com>
2026-01-08 10:35:03 -0300

    categories

--- cb/src/main/java/com/cinebyte/cb/config/TestConfig.java
+++ cb/src/main/java/com/cinebyte/cb/config/TestConfig.java
@@ -40,6 +40,8 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "maria Brown", "maria@gmail.com", "99888888", "123456");
 		User u2 = new User(null, "alex Brwon", "alex@gmail.com", "99777777","123456");
 		
@@ -48,7 +50,6 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- cb/src/main/java/com/cinebyte/cb/services/CategoryService.java
+++ cb/src/main/java/com/cinebyte/cb/services/CategoryService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cinebyte.cb.entities.Category;
-import com.cinebyte.cb.entities.User;
 import com.cinebyte.cb.repositories.CategoryRepository;
 
 @Service
